aeb5fb99b2e757919e486526fc308cfef6794620
Mateo Gonzalez <mateogt0903@gmail.com>
2023-05-01 10:44:46 -0500

    Implementación ubicación actual mapa

--- lib/views/Map/map.dart
+++ lib/views/Map/map.dart
@@ -1,31 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 const MAPBOX_TOKEN =
     'pk.eyJ1IjoibWF0ZW9za2l4IiwiYSI6ImNsaDRheHd4cjF2Y3IzZXA3MXhiYzF3NzQifQ.nHH-TGlUZTKmWtL8TKMIBw';
 
-class MapHome extends StatelessWidget {
+class MapHome extends StatefulWidget {
+  @override
+  _MapHomeState createState() => _MapHomeState();
+}
+
+class _MapHomeState extends State<MapHome> {
+  LatLng myPosition;
+
+  Future<Position> determinePosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('error obteniendo permiso de ubicación');
+      }
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void getCurrentLocation() async {
+    Position myPos = await determinePosition();
+    setState(() {
+      myPosition = LatLng(myPos.latitude, myPos.longitude);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(40.697488, -73.979681),
-        minZoom: 5,
-        maxZoom: 25,
-        zoom: 18,
-      ),
-      nonRotatedChildren: [
-        TileLayer(
-          urlTemplate:
-              'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=$MAPBOX_TOKEN',
-          additionalOptions: {
-            'accessToken': MAPBOX_TOKEN,
-            'id': 'mapbox.streets',
-          },
-        ),
-      ],
-    );
+    return myPosition == null
+        ? Center(child: CircularProgressIndicator())
+        : FlutterMap(
+            options: MapOptions(
+              center: myPosition,
+              minZoom: 5,
+              maxZoom: 25,
+              zoom: 18,
+            ),
+            nonRotatedChildren: [
+              TileLayer(
+                urlTemplate:
+                    'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=$MAPBOX_TOKEN',
+                additionalOptions: {
+                  'accessToken': MAPBOX_TOKEN,
+                  'id': 'mapbox/streets-v12',
+                },
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    point: myPosition,
+                    builder: (context) {
+                      return Container(
+                        child: Icon(Icons.circle,
+                            color: Colors.lightBlue, size: 25),
+                      );
+                    },
+                  )
+                ],
+              ),
+            ],
+          );
   }
 }
