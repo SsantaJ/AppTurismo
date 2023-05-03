1e19638b067cd1bf46d652551693a2b851b7535a
SamuelSanta <samuelsanta2004@gmail.com>
2023-05-02 22:10:35 -0500

    se elimino los dos iconos innecesarios en appbar

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -8,33 +8,4 @@ AppBar HomeAppBar = AppBar(
     "ParchApp",
     style: TextStyle(fontWeight: FontWeight.w900, letterSpacing: 2),
   )),
-  leading: PopupMenuButton(
-    offset: Offset.fromDirection(2, 58),
-    onSelected: (value) {},
-    icon: Icon(Icons.menu),
-    itemBuilder: (context) {
-      return kAppBarMenuOptions
-          .map(
-            (option) => PopupMenuItem(
-              value: option,
-              child: Container(
-                width: 120,
-                child: Text(
-                  option,
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ),
-          )
-          .toList();
-    },
-  ),
-  actions: [
-    IconButton(
-        icon: Icon(
-          Icons.account_circle,
-          size: 36,
-        ),
-        onPressed: () {})
-  ],
 );
